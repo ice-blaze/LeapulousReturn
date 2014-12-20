cd9f5d39880e15b007917be8cbb63cec571e9a1c
ice-blaze <franketip@gmail.com>
2014-12-20 12:03:31 +0100

    Initial commit

---
+++ Assets/CameraAlignement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAlignement : MonoBehaviour {
+
+	OVRCameraRig parentCamera;
+	Vector3 v = new Vector3(280,180,0);
+
+	// Use this for initialization
+	void Start () {
+		parentCamera = GetComponentInParent<OVRCameraRig>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Debug.DrawRay (transform.position, parentCamera.rightEyeCamera.transform.forward, Color.blue);
+//		Camera cam = GetComponentInParent<Camera>();
+//		transform.position = parentCamera.centerEyeAnchor.forward;
+		transform.forward = parentCamera.rightEyeCamera.transform.forward;
+
+		transform.Rotate(v);
+//		Debug.Log(parentCamera.centerEyeAnchor.forward);
+	}
+}
---
+++ Assets/MoveController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+	Camera cam;
+
+	public float speed = 10.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+	public float lookSpeed = 150.0F;
+
+	private Vector3 moveDirection = Vector3.zero;
+
+	float oldValue = 0;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponentInParent<OVRCameraRig>().rightEyeCamera;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mouseRotation = Input.GetAxis("Mouse X")*lookSpeed*Time.deltaTime;
+		transform.Rotate(0,mouseRotation,0);
+
+//		transform.forward = cam.transform.forward;
+
+		CharacterController controller = GetComponent<CharacterController>();
+		if (controller.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+
+	}
+}
